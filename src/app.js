b248fd67ccdfbbc96635f75b48cc61fc894adaa2
Chanhday03 <116981561+chanhday03@users.noreply.github.com>
2023-08-05 09:44:46 +0700

    check permission

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import router from "./routers";
 import mongoose from "mongoose";
+import cors from "cors";
+
 const app = express();
 dotenv.config();
 const { PORT, DB_URL } = process.env;
+
+app.use(cors());
 app.use(express.json());
 mongoose.connect(`${DB_URL}/demo`).then(() => console.log("Connected!"));
 
